a783bc6e3f881001f48af28ccbb3cc07755865f3
cat <youssefannabi@gmail.com>
2020-07-10 13:13:02 +0100

    Updates

---
+++ precache-manifest.1b68f946014c198833100f5613a163df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e1eb663a18108a29d88ffc672a3919",
+    "url": "/react-checkpoint/index.html"
+  },
+  {
+    "revision": "5cdf99f4c957588546a6",
+    "url": "/react-checkpoint/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "199164b60a26a0c1d3ae",
+    "url": "/react-checkpoint/static/css/main.0d5c367c.chunk.css"
+  },
+  {
+    "revision": "5cdf99f4c957588546a6",
+    "url": "/react-checkpoint/static/js/2.d37d6389.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-checkpoint/static/js/2.d37d6389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199164b60a26a0c1d3ae",
+    "url": "/react-checkpoint/static/js/main.1d7f428b.chunk.js"
+  },
+  {
+    "revision": "e3b1e2fadc01eb7f5d1f",
+    "url": "/react-checkpoint/static/js/runtime-main.1d369a3b.js"
+  }
+]);
